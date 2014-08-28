5d909643b5a38381637a4562cd3f4f8759fab896
Rykka <rykka10@gmail.com>
2014-08-28 18:34:20 +0800

    0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.9.0.3',
+    version='0.9.0.4',
 
     description='Preview Rst Document Instantly ',
     long_description=long_description,
