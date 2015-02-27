ba33d90136bd11452b1b67d872441820b51f2c07
xiehuc <xiehuc@gmail.com>
2015-02-27 12:31:13 +0800

    update indent in args.py

--- instant_rst/args.py
+++ instant_rst/args.py
@@ -6,14 +6,14 @@ from instant_rst.config import DEFAULT_FILE, DEFAULT_PORT, DEFAULT_BROWSER, \
 def parse():
     parser = argparse.ArgumentParser(description='Preview rst Instantly')
     parser.add_argument('-f', '--file', dest='filename',
-                default=DEFAULT_FILE,
-                help='The local filename for Converting')
+                        default=DEFAULT_FILE,
+                        help='The local filename for Converting')
     parser.add_argument('-b', '--browser', dest='browser',
-                default='firefox',
-                help='The browser command for viewing')
+                        default='firefox',
+                        help='The browser command for viewing')
     parser.add_argument('-p', '--port', dest='port',
-                default='5676',
-                help='The port for server to use')
+                        default='5676',
+                        help='The port for server to use')
     parser.add_argument('-s', '--static-dir', dest='static_dir',
                         default=STATIC_DIR,
                         help='Directory with static files for rendering')
