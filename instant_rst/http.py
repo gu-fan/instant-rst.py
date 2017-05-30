70904997028c01319e36e69ceef394d30803db77
Anand <anand@igi.tugraz.at>
2017-05-30 23:00:51 +0200

    Running 2to3

--- instant_rst/http.py
+++ instant_rst/http.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-import urllib,urllib2
+import urllib.request, urllib.parse, urllib.error,urllib.request,urllib.error,urllib.parse
 
 def get(url, headers={}):
-    req = urllib2.Request(
+    req = urllib.request.Request(
         url = url,
         headers = headers
     )
-    f = urllib2.urlopen(req)
+    f = urllib.request.urlopen(req)
     con = f.read()
     return {'code':f.getcode(), 'content':con}
 
 def post(url, data, headers={}):
-    req = urllib2.Request(
+    req = urllib.request.Request(
         url = url,
         headers = headers
     )
-    f = urllib2.urlopen(req, urllib.urlencode(data))
+    f = urllib.request.urlopen(req, urllib.parse.urlencode(data))
     con = f.read()
     return {'code':f.getcode(), 'content':con}
 
